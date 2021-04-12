36441af069f7255747376b51dfc9770b67581fef
rveen <rolf.veen@gmail.com>
2021-04-12 20:49:45 +0200

    ...

--- thermal.go
+++ thermal.go
@@ -56,6 +56,16 @@ func PiRH2(rh, temp float64, on bool) float64 {
 	return math.Pow(rh/70, 4.4) * math.Exp(11604*0.9*(1/293-1/(temp+273)))
 }
 
+// Arrhenius25 (in ºC, reference 25ºC)
+func Arrhenius25(ea, temp float64) float64 {
+	return math.Exp(11604.967 * ea * (1/293 - 1/(temp+273)))
+}
+
+// Arrhenius (in K)
+func Arrhenius(ea, t1, t2 float64) float64 {
+	return math.Exp(11604.967 * ea * (1/t1 - 1/t2))
+}
+
 // Temperature cycling, case, Norris-Landzberg model (semiconductor cases)
 func PiTCCase(nc int, time, tdelta, tmax float64) float64 {
 	return 12 * float64(nc) / float64(time) * math.Pow(tdelta/20, 4) * math.Exp(1414*(1/313-1/(tmax+273)))
